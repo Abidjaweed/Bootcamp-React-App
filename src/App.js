ad868e8e578bb91e70fc8b5f3605daf9cce7e823
Abid Jaweed <52130668+Abidjaweed@users.noreply.github.com>
2020-06-07 18:34:00 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1> Hello World </h1>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+        Hello World From Abid Jaweed - React App - Updated.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
