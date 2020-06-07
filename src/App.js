1e37c85bbfc1c98390a7ae5d0ae7ce41a4d75f7f
Abid Jaweed <52130668+Abidjaweed@users.noreply.github.com>
2020-06-07 18:26:07 +0500

    Updated Surge Deployment

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        Hello World From Abid Jaweed - React App.
+        Hello World From Abid Jaweed - React App - Updated 02.
         </p>
         <a
           className="App-link"
