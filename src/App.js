a7433b352e7995df040b8d1923e725a409e4b868
Abid Jaweed <52130668+Abidjaweed@users.noreply.github.com>
2020-06-07 18:12:30 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,6 +10,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>
+          Hello World From Abid Jaweed.
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
